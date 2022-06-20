d1d44216d6cbf4abc23c5a24db852707add0ddab
dizkazzz <klimenko_nekit@mail.ru>
2022-06-20 19:48:47 +0300

    Добавьте файлы проекта.

---
+++ AppWorking/Arguments.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppWorking
+{
+    internal class Arguments
+    {
+        public int Summ (int x , int y)
+        {
+            return x + y;
+        }
+
+        public int Minus (int x , int y)
+        {
+            return x - y;
+        }
+
+        public int Delenie (int x , int y)
+        { 
+
+            try { return x / y; }
+            catch (Exception ex) { return 0; };
+        }
+
+        public int Umn(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ AppWorking/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AppWorking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int _one = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int _two = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите арифметический знак:");
+            string _znak = Console.ReadLine();
+
+            int result;
+
+            switch (_znak)
+            {
+                case "+":
+                    Arguments arguments = new Arguments();
+                    result = arguments.Summ(_one,_two);
+                    Console.WriteLine($"{_one} + {_two} = {result}");
+                    break;
+                case "-":
+                    Arguments arguments1 = new Arguments();
+                    result = arguments1.Minus(_one,_two);
+                    Console.WriteLine($"{_one} - {_two} = {result}");
+                    break;
+                case "*":
+                    Arguments arguments2 = new Arguments();
+                    result = arguments2.Umn(_one,_two);
+                    Console.WriteLine($"{_one} * {_two} = {result}");
+                    break;
+                case "/":
+                    Arguments arguments3 = new Arguments();
+                    result = arguments3.Delenie(_one, _two);
+                    Console.WriteLine($"{_one} / {_two} = {result}");
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели не тот символ");
+                    break;
+
+            }
+        }
+    }
+}
